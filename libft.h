e4ce969cfa0100167c2bf890b90743338daadb2a
Matt <matt@MacBook-Pro-de-MackBook.local>
2021-01-07 23:49:08 +0100

    pushfd

--- libft.h
+++ libft.h
@@ -33,6 +33,10 @@ char	*ft_toupper(char *str);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fr(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
+void	ft_putnbr_fd(int n, int fd);
 
 
 #endif
